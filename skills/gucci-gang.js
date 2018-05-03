12dbafee009404dab6764e86308a2153b33d794d
Ubuntu <ubuntu@ip-172-31-35-165.us-east-2.compute.internal>
2018-05-03 17:00:19 +0000

    refined bots abilities

--- skills/gucci-gang.js
+++ skills/gucci-gang.js
@@ -1,5 +1,5 @@
 var hearsRegex = [
-    /gucci\sgang/i
+    /gucci\s?gang/ig
 ];
 
 var triggers = [
@@ -11,12 +11,33 @@ var triggers = [
 
 var ggText = `Gucci Gang Gucci Gang Gucci Gang Gucci Gang Gucci Gang Gucci Gang Gucci Gang`;
 
+var repeat = function (str, times) {
+    var s = [];
+    for (var i=0; i<times; i++) {
+        s.push(str);
+    }
+    return s.join(' ');
+};
+
 module.exports = function(controller) {
 
     controller.hears(hearsRegex, triggers, function (bot, message) {
-        console.log(message);
+        if (message.match.length >= 8) {
+            bot.reply(message, 'My bitch love do cocaine!');
+        }
+	else if (message.match.length === 1) {
+            bot.reply(message, repeat(message.match[0], 8) + ' :guccigang:');
+        }
+        else {
+            bot.reply(message, '... ' + 
+		repeat(message.match[0], 8 - message.match.length) + 
+		' :guccigang:' );
+        }
 
-        bot.reply(ggText + ':guccigang:');
+    });
+
+    controller.hears(['^lyrics'], triggers.slice(0, 2), function (bot, message) {
+	bot.reply(message, `https://genius.com/Lil-pump-gucci-gang-lyrics`);
     });
 
     controller.hears(['^echo (.*)'], triggers, function (bot, message) {
